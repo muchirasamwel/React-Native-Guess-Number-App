973ea3c643049d125d78e91dd38f91e91d185c0c
Samwel Muchira <muchirasamwel@gmail.com>
2021-07-06 09:29:16 +0300

    Created a new Expo app

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Header() {
+    return (
+        <View>
+            <Text>Guess Number App</Text>
+        </View>
+    )
+}
